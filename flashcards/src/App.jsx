afdcf389b8c3a45af1446293aa0e0a81b0614a76
samade747 <samad.e747@gmail.com>
2024-02-19 13:53:23 +0500

    153pm 1922024

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -2,17 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Firstbox from './components/Firstbox'
 
-function App() {
-  const [count, setCount] = useState(0)
+ 
+
+function App() { 
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Firstbox />
     </>
-      )
+  )
 }
 
 export default App
---
+++ flashcards/src/components/Firstbox.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Firstbox = () => {
+
+
+
+    return(
+     <>
+        <p>samad</p>
+     </>
+    )
+}
+
+export default Firstbox;
