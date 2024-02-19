f288edc5b6ccdfef8e01ac9383dcca6a16af1a36
samade747 <samad.e747@gmail.com>
2024-02-20 00:24:37 +0500

    1224am 20 2 2024

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -8,15 +8,29 @@ import Secondbox from './components/Secondbox'
  
 
 function App() { 
+  
+  const [mainfirst, Setmainfirst] = useState(false)
+
 
   const [textfirst, Settextfirst] = useState(false)
   const [colorfirst, Setcolorfirst] = useState(false)
 
   const [textsecond, Settextsecond] = useState(false)
   const [colorsecond, Setcolorsecond] = useState(false)
+
+
+  const changingColorHandler = () => {
+    
+    Setmainfirst(true)
+}
+
+const revertColorHandler = () => {
+    
+    Setmainfirst(false)
+}
   
   return (
-    <div  className='h-screen flex flex-col' style={{border: "10px solid red"}}>
+    <div  className='h-screen flex flex-col' style={{border: "10px solid red", backgroundColor: mainfirst ? "white" : "black", }}  onMouseOver={changingColorHandler} onMouseLeave={revertColorHandler} >
        <div className="flex flex-1/2 text-black gap-2 m-2">
              <Firstbox textfirst={textfirst} Settextfirst={Settextfirst} colorfirst={colorfirst} Setcolorfirst={Setcolorfirst} />  
              <Secondbox textsecond={textsecond} Settextsecond={Settextsecond} colorsecond={colorsecond} Setcolorsecond={Setcolorsecond} />        
